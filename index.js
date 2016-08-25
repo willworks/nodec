125385b75fec3a98f8a3abc69c7dff115bf27d45
hanjin.zhj <hanjin.zhj@ucwebdeMac-mini.local>
2016-08-25 10:22:40 +0800

    feat: node obj&arr&fn operate in c

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const main = require('./build/Release/main.node');
 const args = process.argv.splice(2);
 
 exports.init = function () {
-  console.log(args.map(x => main.main(x)));
-};
+  // 基本数据类型
+  console.log(args.map(x => main.var(x)));
+  // 数组传递
+  console.log(main.arr([111,222,333]));
+  // 对象传递
+  console.log(main.obj({key:1,key_a:2,c:"sdf",test:[1,2,3]}));
+  // 方法传递
+  const fn = main.func(fn_param);
+  console.log(fn);
+}
+
+function fn_param(){
+  console.log("call fn fn_param by sync!");
+}
 
 this.init();
--- main.cc
+++ main.cc
@@ -2,54 +2,95 @@
 
 namespace main {
 
-using v8::FunctionCallbackInfo;
-using v8::Isolate;
-using v8::Local;
-using v8::Object;
-using v8::String;
-using v8::Number;
-using v8::Array;
-using v8::Boolean;
-using v8::Value;
-using v8::Handle;
-using v8::HandleScope;
+    using v8::Isolate;
+    using v8::Local;
+    using v8::Object;
+    using v8::String;
+    using v8::Number;
+    using v8::Array;
+    using v8::Boolean;
+    using v8::Value;
+    using v8::Integer;
+    using v8::Handle;
+    using v8::HandleScope;
+    using v8::Function;
+    using v8::FunctionCallback;
+    using v8::FunctionCallbackInfo;
 
-void Method(const FunctionCallbackInfo<Value> &args){
-    Isolate * isolate = args.GetIsolate();
-    if (args[0]->IsNumber()){
-        double a = args[0]->NumberValue();
-        Local<Number> return_val =  Number::New(isolate,a+1);
-        args.GetReturnValue().Set(return_val);
-    };
-    if (args[0]->IsBoolean()){
-        bool a = args[0]->BooleanValue();
-        Local<Boolean> return_val = Boolean::New(isolate,!a);
-        args.GetReturnValue().Set(return_val);
-    };
-    if (args[0]->IsString()){
-        Local<String> add_str = String::NewFromUtf8(isolate,"node");
-        add_str = String::Concat(args[0]->ToString(),add_str);
-        args.GetReturnValue().Set(add_str);
-    };
-}
+    void VAR(const FunctionCallbackInfo<Value> &args){
+        // Get the default Isolate created at startup.
+        Isolate * isolate = args.GetIsolate();
+        if (args[0]->IsNumber()){
+            double a = args[0]->NumberValue();
+            Local<Number> return_val =  Number::New(isolate,a+1);
+            args.GetReturnValue().Set(return_val);
+        };
+        if (args[0]->IsBoolean()){
+            bool a = args[0]->BooleanValue();
+            Local<Boolean> return_val = Boolean::New(isolate,!a);
+            args.GetReturnValue().Set(return_val);
+        };
+        if (args[0]->IsString()){
+            Local<String> add_str = String::NewFromUtf8(isolate,"node");
+            add_str = String::Concat(args[0]->ToString(),add_str);
+            args.GetReturnValue().Set(add_str);
+        };
+    }
 
-//Handle<Value> ARRAY(const Arguments& args){
-//    HandleScope scope;
-//    int row_index;
-//    Local<Array> dataArray=Array::New(10);      //dataArray=args[0].AsArray;
-//    for(row_index=0;row_index<10;row_index++)
-//    {
-//        Local<Value> cell=Number::New(args[row_index]->NumberValue()+1);
-//        dataArray->Set(Integer::New(row_index),cell);
-//    }
-//    return scope.Close(dataArray);
-//}
+    // 给我们的对象附加一个函数
+    void cb(const FunctionCallbackInfo<Value> &args){
+        printf("this is called in addon module!\n");
+    }
 
-void init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "main", Method);
-//  NODE_SET_METHOD(exports, "array", ARRAY);
-}
+    void OBJ(const FunctionCallbackInfo<Value> &args){
+        // Get the default Isolate created at startup.
+        Isolate * isolate = args.GetIsolate();
+        // 需要返回的v8对象
+        Local<Object> v8Obj = Object::New(isolate);
+        // 取得函数的指针
+        FunctionCallback out_cb = cb;
 
-NODE_MODULE(addon, init)
+        // 给v8对象设置一个键名为key值为111的字段
+        v8Obj->Set(String::NewFromUtf8(isolate,"key"),Integer::New(isolate,111));
+        // 给v8对象设置一个键名为run_cplus_fn的函数
+        // v8Obj->Set(String::NewFromUtf8(isolate,"run_cplus_fn"),out_cb);
+
+        // 如果传递的参数是v8::Object类型，再给对象设置一个键名为js_obj_keys的数组，数组值为js对象的key集合
+        if(args[0]->IsObject()){
+            Local<Object>  received_v8_obj = args[0]->ToObject();
+            Local<Array> keys = received_v8_obj->GetOwnPropertyNames();
+            v8Obj->Set(String::NewFromUtf8(isolate,"js_obj_keys"),keys);
+        }
+        args.GetReturnValue().Set(v8Obj);
+    }
+
+    void ARR(const FunctionCallbackInfo<Value> &args){
+        Isolate* isolate = args.GetIsolate();
+        Local<Array>tArr = Local<Array>::Cast(args[0]);
+        args.GetReturnValue().Set(tArr->Get(0)->NumberValue());
+    }
+
+    void FUNC(const FunctionCallbackInfo<Value> &args){
+        // Get the default Isolate created at startup.
+        Isolate * isolate = args.GetIsolate();
+
+        if(args[0]->IsFunction()){
+            Local<Function> js_fn = Local<Function>::Cast(args[0]);
+            for(int i=0;i<11;i++){
+                printf("%d\n", i);
+            }
+            js_fn->Call(Null(isolate),0,NULL);
+            args.GetReturnValue().Set(js_fn);
+        }
+    }
+
+    void init(Local<Object> exports) {
+      NODE_SET_METHOD(exports, "var", VAR);
+      NODE_SET_METHOD(exports, "obj", OBJ);
+      NODE_SET_METHOD(exports, "arr", ARR);
+      NODE_SET_METHOD(exports, "func", FUNC);
+    }
+
+    NODE_MODULE(addon, init)
 
 }  // namespace main
